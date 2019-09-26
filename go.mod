25511fa10b2ed4d6deafd256f866ea3bcfe5af29
Eric Rumble <eric.rumble@terminus.com>
2019-09-26 11:08:28 -0400

    call sts.GetCallerIdentity

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/erumble/k8s-iam-test
 
 go 1.13
 
-require github.com/jessevdk/go-flags v1.4.0
+require (
+	github.com/aws/aws-sdk-go v1.24.6
+	github.com/jessevdk/go-flags v1.4.0
+)
--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/jessevdk/go-flags"
 )
 
 // Opts struct to allow input from user
 var Opts struct {
 	Port string `short:"p" long:"port" description:"Port to run server on" required:"false" default:"8080"`
+}
+
+type stsAPI interface {
+	GetCallerIdentity(*sts.GetCallerIdentityInput) (*sts.GetCallerIdentityOutput, error)
 }
 
 func main() {
@@ -24,15 +30,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	svc := sts.New(session.New())
+
 	log.Println("starting server, listening on port " + Opts.Port)
 
-	http.HandleFunc("/", EchoHandler)
+	http.HandleFunc("/", echoHandler())
+	http.HandleFunc("/getCallerIdentity", getCallerIdentityHandler(svc))
 	http.ListenAndServe(":"+Opts.Port, nil)
 }
 
-// EchoHandler echos back the request as a response
-func EchoHandler(writer http.ResponseWriter, request *http.Request) {
-
+func echo(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("Echoing back request made to %s to client (%s)", request.URL.Path, request.RemoteAddr)
 
 	writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -41,4 +48,26 @@ func EchoHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
 
 	request.Write(writer)
+}
+
+func echoHandler() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		echo(writer, request)
+	}
+}
+
+func getCallerIdentityHandler(stsClient stsAPI) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		input := &sts.GetCallerIdentityInput{}
+
+		// Only log the output from the GetCallerIdentity API Call, no need to share the info with the world.
+		res, err := stsClient.GetCallerIdentity(input)
+		if err != nil {
+			log.Printf("Error calling sts.GetCallerIdentity: %v", err)
+		}
+
+		log.Printf("sts.GetCallerIdentityResults: %v", res)
+
+		echo(writer, request)
+	}
 }
